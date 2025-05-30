0d71e8f382c17d31537e8e5d2cece34071458a45
mmalla642 <mothishree_mallaiyanjothimani@comcast.com>
2025-05-30 07:20:00 +0000

    RDKB-36610: Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- include/wifi_base.h
+++ include/wifi_base.h
@@ -344,6 +344,7 @@ typedef struct {
     wifi_mon_stats_args_t     args;
     wifi_channelBandwidth_t channelWidth;
     wifi_freq_bands_t   band;
+    bool nop_up_status;
     unsigned int nop_up_channel;
 } __attribute__((packed)) wifi_mon_stats_config_t;
 
