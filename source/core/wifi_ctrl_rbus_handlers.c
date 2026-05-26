3d996a07086e9a751379adf6e8f4d45997796995
mmalla642 <mothishree_mallaiyanjothimani@comcast.com>
2026-05-26 10:06:59 +0000

    RDKB-36610: Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- source/core/wifi_ctrl_rbus_handlers.c
+++ source/core/wifi_ctrl_rbus_handlers.c
@@ -587,7 +587,20 @@ bus_error_t webconfig_init_data_get_subdoc(char *event_name, raw_data_t *p_data,
             sizeof(wifi_hal_capability_t));
         data.u.decoded.num_radios = num_of_radios;
         // tell webconfig to encode
-	webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_dml);
+        if (webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_dml) !=
+            webconfig_error_none) {
+            wifi_util_error_print(WIFI_CTRL, "%s:%d webconfig_encode failed\n", __func__,
+                __LINE__);
+            webconfig_data_free(&data);
+            return bus_error_invalid_operation;
+        }
+
+        if (data.u.encoded.raw == NULL) {
+            wifi_util_error_print(WIFI_CTRL, "%s:%d encoded raw data is NULL\n", __func__,
+                __LINE__);
+            webconfig_data_free(&data);
+            return bus_error_invalid_operation;
+        }
 
         uint32_t str_size = (strlen(data.u.encoded.raw) + 1);
         p_data->data_type = bus_data_type_string;
@@ -616,10 +629,22 @@ bus_error_t webconfig_init_data_get_subdoc(char *event_name, raw_data_t *p_data,
 	if (ctrl->dev_type != dev_subtype_pod) {
 		memcpy((unsigned char *)&data.u.decoded.config, (unsigned char *)&mgr->global_config,
 				sizeof(wifi_global_config_t));
-		webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_dml);
-	} else {
-		webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_mesh_sta);
-	}
+                if (webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_dml)) {
+                    wifi_util_error_print(WIFI_CTRL, "MJ %s:%d webconfig_encode failed\n", __func__,
+                        __LINE__);
+                    webconfig_data_free(&data);
+                    return bus_error_invalid_operation;
+                }
+        } else {
+            webconfig_encode(&ctrl->webconfig, &data, webconfig_subdoc_type_mesh_sta);
+        }
+
+        if (data.u.encoded.raw == NULL) {
+            wifi_util_error_print(WIFI_CTRL, "MJ %s:%d encoded raw data is NULL\n", __func__,
+                __LINE__);
+            webconfig_data_free(&data);
+            return bus_error_invalid_operation;
+        }
 
         uint32_t str_size = (strlen(data.u.encoded.raw) + 1);
         p_data->data_type = bus_data_type_string;
