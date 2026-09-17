c97a0f5a817771bde9c43b425d5026a66714ccd2
mmalla642 <mothishree_mallaiyanjothimani@comcast.com>
2026-09-17 05:30:23 +0000

    RDKB-36610: Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- source/db/wifi_db.h
+++ source/db/wifi_db.h
@@ -160,8 +160,6 @@ void wifidb_init_rfc_config_default(wifi_rfc_dml_parameters_t *config);
 int wifidb_update_wifi_vap_config(int radio_index, wifi_vap_info_map_t *config,
     rdk_wifi_vap_info_t *rdk_config);
 
-int wifidb_get_default_mld_link_id(int band);
-
 #ifdef __cplusplus
 }
 #endif
